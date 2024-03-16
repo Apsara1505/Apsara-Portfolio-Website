69b40471cfdb271d14e35d0c10b656cf9632d12f
Apsara1505 <prabodi999.desilva@gmail.com>
2024-03-16 15:51:40 +0530

    make css changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,70 @@
 /* ============================ SHOW SIDEBAR ========================= */
-
+document.addEventListener("DOMContentLoaded", function() {
+    const navMenu = document.getElementById('sidebar'),
+          navToggle = document.getElementById('nav-toggle'),
+          navClose = document.getElementById('nav-close')
 /* ============================  SIDEBAR SHOW ========================= */
-/* Validate if constant existS */
+
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-sidebar")
+    })
+}
+
 
 /* ============================ SIDEBAR HIDDEN ========================= */
-/* Validate if constant existS */
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-sidebar")
+    })
+}
+
+})
+
+
+/* ============================== sidebar active link ========================*/
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav_link');
+
+    // Function to set the initial active link
+    function setInitialActiveLink() {
+        navLinks.forEach(link => {
+            link.classList.remove('active-link');
+        });
+        // Add 'active-link' class to the Home link
+        document.querySelector('.nav_link[href="#home"]').classList.add('active-link');
+    }
+
+    // Call the function to set the initial active link
+    setInitialActiveLink();
+
+    window.addEventListener('scroll', function() {
+        let current = '';
+
+        // Iterate over each section to determine which one is in view
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (pageYOffset >= sectionTop - sectionHeight / 3 && 
+                pageYOffset < sectionTop + sectionHeight) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        // Remove the 'active-link' class from all navigation links
+        navLinks.forEach(link => {
+            link.classList.remove('active-link');
+        });
+
+        // Add the 'active-link' class to the navigation link corresponding to the current section
+        const currentNavLink = document.querySelector('.nav_link[href="#' + current + '"]');
+        if (currentNavLink) {
+            currentNavLink.classList.add('active-link');
+        }
+    });
+});
+
 
 /* ============================ SKILLS TABS ========================= */
 document.addEventListener("DOMContentLoaded", function() {
@@ -54,7 +114,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-/* ============================ Link Active Work ========================= */
+/* ============================ Active Work item ========================= */
 document.addEventListener('DOMContentLoaded', function () {
     let linkWork = document.querySelectorAll('.work_item')
     
@@ -120,11 +180,8 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener('DOMContentLoaded', function () {
     let swiper = new Swiper(".testimonials_container", {
         spaceBetween: 24,
-        centeredSlides: true,
-        autoplay: {
-          delay: 4000,
-          disableOnInteraction: false,
-        },
+        loop: true,
+        grabCursor: true,
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
